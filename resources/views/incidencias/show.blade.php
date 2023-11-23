89624bcdb08c1b96814b271a06619f42ce828a12
antoniolc11 <antoniolc11@hotmail.com>
2023-11-23 18:40:38 +0100

    Condicional para mostrar el botón de parte de trabajo.

--- resources/views/incidencias/show.blade.php
+++ resources/views/incidencias/show.blade.php
@@ -56,7 +56,7 @@
                         </div>
                     </th>
                 </tr>
-                
+
                 <tr class="border-t border-gray-200">
                     <th class="text-left w-48 text-xs font-medium text-gray-500 uppercase tracking-wider">Número de
                         Incidencia:</th>
@@ -271,10 +271,15 @@
                         <button data-modal-hide="defaultModal" type="button"
                             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Aceptar</button>
 
-                        <a href="#" target="_blank" class="btn btn-primary">
-                            <button data-modal-hide="defaultModal" type="button"
-                                class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Parte de trabajo</button>
-                        </a>
+
+                        @if ($incidencia->estado_id == 3)
+                            <a href="#" target="_blank" class="btn btn-primary">
+                                <button data-modal-hide="defaultModal" type="button"
+                                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Parte
+                                    de trabajo</button>
+                            </a>
+                        @endif
+
                     </div>
                 </div>
             </div>
